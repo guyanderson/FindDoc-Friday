ab990af1843602c1635c10fabbf7f0e0b18b085d
guy <luckythelich@hotmail.com>
2017-06-30 11:23:03 -0700

    not recognizing doctor as a constructor.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,14 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+var displayDoctor = function(name) {
+    $('.showDoctor').append(name);
+}
+
+$(document).ready(function() {
+    $('#medical-condition').click(function() {
+        var newDoctor = new Doctor();
+        var medicalIssue = $('condition').val();
+        //$('#condition').val("");
+        newDoctor.getDoctors(medicalIssue);
+    });
+});
