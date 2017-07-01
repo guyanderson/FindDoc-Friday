ebb4ce8edebb531b77b40c3b431f7b22c8fee9ec
guy <luckythelich@hotmail.com>
2017-07-01 10:53:42 -0700

    combine js files into 1. start trouble shooting. no longer getting2 loads when refreshing page.

--- old/olddoctor-interface.js
+++ old/olddoctor-interface.js
@@ -1,23 +1,17 @@
-var apiKey = require('./../.env').apiKey;
-
-function Doctor(){
-}
-
-Doctor.prototype.getDoctors = function(medicalIssue) {
-};
+var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(name) {
     $('.showDoctor').append(name);
 }
 
 $(document).ready(function() {
-    var newDoctor = new Doctor();
+//    var newDoctor = new Doctor();
     alert('twice?');
     $('#medical-condition').click(function() {
         alert("hi");
         var medicalIssue = $('condition').val();
         $('#condition').val("");
-        newDoctor.getDoctors(medicalIssue);
+//        newDoctor.getDoctors(medicalIssue);
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=rash&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=b967904c63b94561bd3525a4b3b32656' + '&format=json').then(function(response) {
         alert('bye');
             displayDoctor(response);
@@ -27,5 +21,3 @@ $(document).ready(function() {
         });
     });
 });
-
-exports.doctorModule = Doctor;
