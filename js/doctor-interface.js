7093b9facdb8ba28db27c02c4a5c28e5d9385e2a
guy <luckythelich@hotmail.com>
2017-07-01 15:44:26 -0700

    code is now split on 2 JS files. code still works. Still getting a constructor is not a constructor error. This error is why I put the code onto 1 page to trouble shoot. It works on 1 page, then errors with 2 pages(2 js files). THis error was also present in the code created in class. Feels like I just wasted 2 days on an error thats wasn't an error, as it is using my constructor to construct. THe double page request error that was present before the JS page merge trouble shooting is gone. Not sure what caused/solved that.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,22 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var DoctorList = require('./../js/doctor.js').doctorModule;
+
+
+var displayDoctors = function(result) {
+    $('.showDoctor').empty();
+    for(var i = 0; i <=result.data.length; i++) {
+        $('.showDoctor').append("<p><img src=" + result.data[i].profile.image_url + "></p>");
+        $('.showDoctor').append("<p>" + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + "</p>");
+    }
+}
+
+
+$(document).ready(function() {
+    var newDoctorList = new DoctorList();
+    $('#medical-condition').click(function() {
+        var medicalIssue = $('#condition').val();
+        $('#condition').val("");
+        newDoctorList.getDoctors(medicalIssue);
+    });
+});
+
+exports.doctorModule = displayDoctors;
--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,8 @@
 var apiKey = require('./../.env').apiKey;
+var displayDoctors = require('./../js/doctor-interface.js').doctorModule;
 
 function DoctorList() {
 }
-
-// DoctorList.prototype.getDoctors = function(medicalIssue) {
-// };
 
 DoctorList.prototype.getDoctors = function(medicalIssue) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
@@ -14,48 +12,5 @@ DoctorList.prototype.getDoctors = function(medicalIssue) {
         console.log("failed");
     });
 };
-
-//=============================================================================
-
-var displayDoctors = function(result) {
-    $('.showDoctor').empty();
-    for(var i = 0; i <=result.data.length; i++) {
-        $('.showDoctor').append("<p><img src=" + result.data[i].profile.image_url + "></p>");
-        $('.showDoctor').append("<p>" + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + "</p>");
-    }
-}
-
-
-$(document).ready(function() {
-    var newDoctorList = new DoctorList();
-    $('#medical-condition').click(function() {
-        var medicalIssue = $('#condition').val();
-        $('#condition').val("");
-        newDoctorList.getDoctors(medicalIssue);
-    });
-});
-
-
-
-
-
-// $(document).ready(function() {
-//     var newDoctorList = new DoctorList();
-//     //alert('twice?');
-//     $('#medical-condition').click(function() {
-//         //alert("hi");
-//         var medicalIssue = $('#condition').val();
-//         $('#condition').val("");
-//         newDoctorList.getDoctors(medicalIssue);
-//         $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
-//         //alert(medicalIssue);
-//         //alert('bye');
-//             displayDoctors(result);
-//             console.log(result);
-//         }).fail(function(error){
-//             console.log("failed");
-//         });
-//     });
-// });
 
 exports.doctorModule = DoctorList;
