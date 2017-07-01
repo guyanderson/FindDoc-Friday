d1bf7e2ad395a8ebda9b6e5f3a5a833670aceb38
guy <luckythelich@hotmail.com>
2017-07-01 11:15:13 -0700

    console log able to return 2 doctors when typing in symptom -rash. 1 of the doctors is named doctor rash. That can't be a good name for a doctor.

--- js/doctor.js
+++ js/doctor.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=rash&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=b967904c63b94561bd3525a4b3b32656' + '&format=json').then(function(response) {
         alert('bye');
             displayDoctor(response);
-            console.log(result);
+            console.log(response);
         }).fail(function(error){
             console.log("fail");
         });
