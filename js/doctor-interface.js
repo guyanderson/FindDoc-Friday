299b70395e62197353da357e17806fe8e4348fdc
guy <luckythelich@hotmail.com>
2017-06-30 18:02:52 -0700

    code is not working. unable to solve constructor is not a constructor issue. page is being loaded twice on serve start or refresh.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,10 +5,19 @@ var displayDoctor = function(name) {
 }
 
 $(document).ready(function() {
+//    var newDoctor = new Doctor();
+    alert('twice?');
     $('#medical-condition').click(function() {
-        var newDoctor = new Doctor();
+        alert("hi");
         var medicalIssue = $('condition').val();
-        //$('#condition').val("");
-        newDoctor.getDoctors(medicalIssue);
+        $('#condition').val("");
+//        newDoctor.getDoctors(medicalIssue);
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=rash&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=b967904c63b94561bd3525a4b3b32656' + '&format=json').then(function(response) {
+        alert('bye');
+            displayDoctor(response);
+            console.log(result);
+        }).fail(function(error){
+            console.log("fail");
+        });
     });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,15 +2,16 @@ var apiKey = require('./../.env').apiKey;
 var displayDoctor = require('./../js/doctor-interface.js').displayDoctor;
 
 
-Doctor = function(){
+function Doctor(){
 }
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-            displayDoctor(data.profile.first_name);
-        }).fail(function(error){
-            console.log("fail");
-        });
+    // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey + '&format=json').then(function(response) {
+    //         displayDoctor(response);
+    //         //console.log(result);
+    //     }).fail(function(error){
+    //         console.log("fail");
+    //     });
 };
 
 exports.doctorModule = Doctor;
