6a8a9ea71ae869cf3279379375ea5561eee51aa5
guy <luckythelich@hotmail.com>
2017-07-01 13:12:45 -0700

    changed range to 100 miles inorder to get better testing. able to append an array of doctor first names to screen.

--- js/doctor.js
+++ js/doctor.js
@@ -1,26 +1,32 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(){
+function DoctorList(){
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+DoctorList.prototype.getDoctors = function(medicalIssue) {
 };
 
-var displayDoctor = function(name) {
-    $('.showDoctor').append(name);
+var displayDoctors = function(response) {
+    $('.showDoctor').empty();
+    for(var i = 0; i <=response.data.length; i++) {
+        $('.showDoctor').append("<p>" + response.data[i].profile.first_name + "</p>");
+
+    }
+    $('.showDoctor').append("<p>Ta da!</p>");
 }
 
 $(document).ready(function() {
-    var newDoctor = new Doctor();
+    var newDoctorList = new DoctorList();
     alert('twice?');
     $('#medical-condition').click(function() {
         alert("hi");
-        var medicalIssue = $('condition').val();
-        $('#condition').val("");
-        newDoctor.getDoctors(medicalIssue);
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=rash&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=b967904c63b94561bd3525a4b3b32656' + '&format=json').then(function(response) {
+        var medicalIssue = $('#condition').val();
+        //$('#condition').val("");
+        newDoctorList.getDoctors(medicalIssue);
+     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+         alert(medicalIssue);
         alert('bye');
-            displayDoctor(response);
+            displayDoctors(response);
             console.log(response);
         }).fail(function(error){
             console.log("fail");
@@ -28,4 +34,7 @@ $(document).ready(function() {
     });
 });
 
-exports.doctorModule = Doctor;
+exports.doctorModule = DoctorList;
+
+
+//('https://api.betterdoctor.com/2016-03-01/doctors?query=rash&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=b967904c63b94561bd3525a4b3b32656' + '&format=json')
